e49c471c62f9e3ea58876df80bab678bc9c338b8
WRP Labs <elsel@bitbucket.org>
2017-02-24 08:10:57 +0100

    Initial commit

---
+++ main.bundle.js
@@ -0,0 +1,294 @@
+webpackJsonp([1,4],{
+
+/***/ 290:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 290;
+
+
+/***/ }),
+
+/***/ 291:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(378);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(399);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(402);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/main.js.map
+
+/***/ }),
+
+/***/ 398:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        var _this = this;
+        this.title = 'Simple form :';
+        this.formTitle = 'Inscription';
+        this.goSignin = function () {
+            _this.formTitle = "Inscription";
+        };
+        this.goLogin = function () {
+            _this.formTitle = "Connexion";
+        };
+    }
+    AppComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__(459),
+            styles: [__webpack_require__(456)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/app.component.js.map
+
+/***/ }),
+
+/***/ 399:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(167);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_http__ = __webpack_require__(374);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_forms__ = __webpack_require__(250);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_common__ = __webpack_require__(122);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(398);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__form1_form1_component__ = __webpack_require__(401);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__form1_form1_component__["a" /* Form1Component */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_forms__["a" /* ReactiveFormsModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_http__["a" /* HttpModule */],
+                __WEBPACK_IMPORTED_MODULE_4__angular_common__["a" /* CommonModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/app.module.js.map
+
+/***/ }),
+
+/***/ 400:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CustomValidators; });
+var CustomValidators = (function () {
+    function CustomValidators() {
+    }
+    CustomValidators.noWhiteSpaces = function (c) {
+        return c.value.trim() != "" ? null : {
+            validateWhiteSpaces: {
+                valid: false
+            }
+        };
+    };
+    CustomValidators.onlyLettersPattern = /^[a-zA-Z\- àâäèéêëîïôœùûüÿçÀÂÄÈÉÊËÎÏÔŒÙÛÜŸÇ]*$/;
+    CustomValidators.emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return CustomValidators;
+}());
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/custom-validators.js.map
+
+/***/ }),
+
+/***/ 401:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_forms__ = __webpack_require__(250);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__custom_validators__ = __webpack_require__(400);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Form1Component; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var Form1Component = (function () {
+    function Form1Component(fb) {
+        this.fb = fb;
+        this.signinForm = this.fb.group({
+            lstName: ['', [__WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].required, __WEBPACK_IMPORTED_MODULE_2__custom_validators__["a" /* CustomValidators */].noWhiteSpaces, __WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].pattern(__WEBPACK_IMPORTED_MODULE_2__custom_validators__["a" /* CustomValidators */].onlyLettersPattern)]],
+            fstName: ['', __WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].pattern(__WEBPACK_IMPORTED_MODULE_2__custom_validators__["a" /* CustomValidators */].onlyLettersPattern)],
+            gender: [null, __WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].required],
+            power: ['', __WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].required],
+            comments: ['', [__WEBPACK_IMPORTED_MODULE_1__angular_forms__["b" /* Validators */].required, __WEBPACK_IMPORTED_MODULE_2__custom_validators__["a" /* CustomValidators */].noWhiteSpaces]],
+            newsletter: [false]
+        });
+        this.title = 'Simple form :';
+        this.formData = { newsletter: false, power: '' };
+        this.powers = [{ label: 'Emploi 1', value: '1' }, { label: 'Emploi 2', value: '2' },
+            { label: 'Emploi 3', value: '3' }, { label: 'Emploi 4', value: '4' }];
+        this.testFct = function () {
+            //console.log(this.mockForm);
+            console.log('BLLLLAAH', this.signinForm.get('lstName'));
+            console.log('=> Signin form value', this.signinForm.value);
+        };
+    }
+    Form1Component.prototype.ngOnInit = function () {
+    };
+    __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["l" /* Input */])(), 
+        __metadata('design:type', Object)
+    ], Form1Component.prototype, "formTitle", void 0);
+    Form1Component = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["U" /* Component */])({
+            selector: 'app-form1',
+            template: __webpack_require__(460),
+            styles: [__webpack_require__(457)]
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_forms__["c" /* FormBuilder */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__angular_forms__["c" /* FormBuilder */]) === 'function' && _a) || Object])
+    ], Form1Component);
+    return Form1Component;
+    var _a;
+}());
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/form1.component.js.map
+
+/***/ }),
+
+/***/ 402:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=/Users/Nicow/Dev/HSBC/POC/simple-form_play/src/environment.js.map
+
+/***/ }),
+
+/***/ 456:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(120)();
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 457:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(120)();
+// imports
+
+
+// module
+exports.push([module.i, ".ng-valid:not(form).ng-dirty  {\n  border-left: 5px solid #42A948; /* green */\n}\n\n.ng-invalid:not(form).ng-dirty  {\n  border-left: 5px solid #a94442; /* red */\n}\n\n\ninput[type=\"text\"] {\n  padding-left: 5px;\n}\n\n.label-padded{\n  padding-left: 10px;\n}\n\ntextarea{\n  padding-left: 5px;\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 459:
+/***/ (function(module, exports) {
+
+module.exports = "\n<div class=\"center-align\">\n  <h1>\n    {{title}}\n  </h1>\n  <div>\n    <a (click)=\"goSignin()\">Inscription</a> <a (click)=\"goLogin()\">Connexion</a>\n  </div>\n  <app-form1 [formTitle]=\"formTitle\"></app-form1>\n</div>\n"
+
+/***/ }),
+
+/***/ 460:
+/***/ (function(module, exports) {
+
+module.exports = " \n  <div class=\"container white\">\n    <div class=\"card left-align\">\n      <div class=\"card-content\">\n          <h3>{{formTitle}}</h3>\n          <div class=\"row\">\n        \n        <!--   <button (click)=\"testFct()\"> Test </button> -->\n            <!-- Start form -->\n            <form \n              name=\"testForm\" \n              #testForm \n              action=\"http://troispointzero.fr\" \n              target=\"_blank\" \n              method=\"GET\" \n              class=\"col s12\"\n              [formGroup]=\"signinForm\"\n            >\n          \n              <!-- Text input field -->\n              <div class=\"row\">\n                <div class=\"input-field col s12\">\n                  \n                  <input \n                    id=\"lstName\"\n                    type=\"text\" \n                    name=\"lstName\" \n                    formControlName=\"lstName\"\n                  />\n                  <label for=\"lstName\" class=\"label-padded\">Nom : *</label>\n                  <div [hidden]=\"signinForm.controls.lstName.valid || signinForm.controls.lstName.pristine\" class=\"red-text\">\n                    Votre nom est requis.\n                  </div>\n\n                </div>\n              </div>\n\n              <div class=\"row\">\n                <div class=\"input-field col s12\">\n                  \n                  <input \n                    id=\"fstName\"\n                    type=\"text\" \n                    name=\"fstName\" \n                    formControlName=\"fstName\"\n                  />\n                  <label for=\"fstName\" class=\"label-padded\">Prénom : </label>\n                </div>\n              </div>\n\n              <!-- Radio input field -->\n              <div class=\"row\">\n                <div class=\"col s12\">\n                  <p>Vous êtes : *</p>\n                  <p>\n                    <input \n                      name=\"gender\" \n                      type=\"radio\" \n                      id=\"gender1\" \n                      value=\"h\"\n                      formControlName=\"gender\"\n                    />\n                    <label for=\"gender1\">Un homme</label>\n                  </p>\n                  <p>\n                    <input \n                      name=\"gender\" \n                      type=\"radio\" \n                      id=\"gender2\" \n                      value=\"f\"\n                      formControlName=\"gender\"\n                    />\n                    <label for=\"gender2\">Une femme</label>\n                  </p>\n                  <div [hidden]=\"signinForm.controls.gender.valid || signinForm.controls.gender.pristine\" class=\"red-text\">\n                    La puissance est requise.\n                  </div>\n                </div>\n              </div>\n\n              <!-- Select input field -->\n              <div class=\"row\">\n                <div class=\"input-field col s12\">\n                  <label for=\"power\">Emploi : *</label>\n                  <br><br>\n                  <select \n                    class=\"browser-default\"\n                    id=\"power\" \n                    formControlName=\"power\"\n                  >\n                    <option value=\"\" disabled selected>Choisissez votre option</option>\n                    <option *ngFor=\"let pow of powers\" [value]=\"pow.value\" [attr.selected]=\"pow.value == '2' ? true : null\">{{pow.label}}</option>\n                  </select>\n                  \n                  <div [hidden]=\"signinForm.controls.power.valid || signinForm.controls.power.pristine\" class=\"red-text\">\n                    La puissance est requise.\n                  </div>\n                </div>\n              </div>\n\n\n              <!-- Text area input field -->\n              <div class=\"row\">\n                <div class=\"input-field col s12\">\n                  <textarea \n                    id=\"comments\" \n                    name=\"comments\"\n                    class=\"materialize-textarea\" \n                    formControlName=\"comments\"\n                  >\n                  </textarea>\n                  <label for=\"comments\" class=\"label-padded\">Commentaires</label>\n                  <div [hidden]=\"signinForm.controls.comments.valid || signinForm.controls.comments.pristine\" class=\"red-text\">\n                    Les commentaires sont requis.\n                  </div>\n                </div>\n              </div>\n                \n              <!-- Checkbox input field -->\n              <div class=\"row\">\n                <div class=\"col s12\">\n                  <input \n                    type=\"checkbox\" \n                    class=\"filled-in\" \n                    id=\"filled-in-box\" \n                    name=\"newsletter\"\n                    formControlName=\"newsletter\"\n                  />\n                  <label for=\"filled-in-box\">M'inscire à la newsletter</label>\n                </div>\n              </div>\n\n              <br>\n              <div class=\"right-align\">\n                  <button \n                    class=\"btn waves-effect waves-light  teal lighten-2\" \n                    type=\"submit\" \n                    name=\"action\"\n                    [disabled]=\"!signinForm.valid\"\n                    (click)=\"testForm.submit()\"\n                  >\n                    Envoyer\n                  </button>\n              </div>\n            </form>\n            <!-- /End form -->\n            \n          </div>\n\n      </div>\n    </div>\n  </div>\n"
+
+/***/ }),
+
+/***/ 473:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(291);
+
+
+/***/ })
+
+},[473]);
+//# sourceMappingURL=main.bundle.js.map
